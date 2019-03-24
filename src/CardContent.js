7cec2c7cf9a761d3f9d7f1e633cd939d3c03de05
Guandjoy <guandjoy@gmail.com>
2019-03-24 23:30:42 +0200

    Improve image load

--- src/CardContent.js
+++ src/CardContent.js
@@ -1,10 +1,26 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
+
+const randomHeight = () => Math.floor(Math.random() * 256) + 150
 
 function CardContent(props) {
   const cardRef = useRef();
+  const [isLoad, setIsLoad] = useState(false)
+  const height = randomHeight()
+  useEffect(() => {
+
+  })
   return (
-    <div ref={cardRef}>
-      <img src={props.image} width="100%" />
+    <div ref={cardRef} className="card" style={{
+    	
+    	minHeight: isLoad ? null : height
+    }} >
+    {isLoad ? null : <div style={{position: 'absolute', width: '256px', height: '100%', }}>loading</div>}
+    <img src={props.image} onLoad={() => setIsLoad(true)} style={{
+    	opacity: isLoad ? '1' : '0.1',
+    	width: '100%',
+    	height: isLoad ? null : height
+    }}/>
+      
     </div>
   );
 }
